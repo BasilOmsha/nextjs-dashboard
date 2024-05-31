29e83cadd99506672fd4038ae71b114ecf560391
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-05-31 23:45:46 +0300

    chore: Add dotenv configuration

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,6 +9,7 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+require('dotenv').config({ path: `${__dirname}/../../../../keys/nextjs-dashboard/.env` });
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
