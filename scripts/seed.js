674e292f9693cc906258bb153f16f6875e8a14f0
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-05-31 23:39:42 +0300

    chore: Update dashboard page with revenue chart and latest invoices

--- scripts/seed.js
+++ scripts/seed.js
@@ -6,6 +6,7 @@ const {
   users,
 } = require('../app/lib/placeholder-data.js');
 const bcrypt = require('bcrypt');
+require('dotenv').config({ path: `${__dirname}/../../../keys/nextjs-dashboard/.env` });
 
 async function seedUsers(client) {
   try {
