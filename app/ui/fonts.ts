1cfbc0c0858335c584d9365d57a1f3bfc601b80d
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-05-31 10:11:47 +0300

    chore: Add inter and lusitana font imports for better typography

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+// Export another font called 'Lusitana' from Google Fonts
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
+
