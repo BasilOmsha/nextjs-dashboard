f5ca8fad286bf42d602c48042792c22bc817545d
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-06-01 16:53:31 +0300

    chore: Update pagination component to use search parameters

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -20,6 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams); //URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
+    params.set('page', '1');
+    
     if (term) {
       params.set('query', term);
       // console.log(term);
