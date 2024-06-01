251f76d68cd99f33ff666aa96af73eb4068e7af5
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-06-01 16:12:31 +0300

    Search functionality added

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,34 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+/*
+*useSearchParams Allows you to access the parameters of the current URL. For example, the search params for this URL /dashboard/invoices?page=1&query=pending would look like 
+ this: {page: '1', query: 'pending'}.
+*usePathname - Lets you read the current URL's pathname. For example, for the route /dashboard/invoices, usePathname would return '/dashboard/invoices'.
+*useRouter - Enables navigation between routes within client components programmatically. There are multiple methods you can use.
+*/
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'; //
+import { useDebouncedCallback } from 'use-debounce'; // useDebouncedCallback is a hook that debounces a callback. It's useful when you want to delay the execution of a function until a certain amount of time has passed since the last time it was called.
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams); //URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
+    if (term) {
+      params.set('query', term);
+      // console.log(term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +37,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()} // Insures that the input field is updated with the current search query.
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
