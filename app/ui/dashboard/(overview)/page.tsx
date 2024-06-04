4d4d3e05da4033a98aacf9ab969ba10393abb507
Basel <107993017+BasilOmsha@users.noreply.github.com>
2024-06-04 09:20:41 +0300

    Login capabilities. Meta data

--- app/ui/dashboard/(overview)/page.tsx
+++ app/ui/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import { fetchCardData } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue(); //Moved to RevenueChart.tsx to use Suspense for streaming data loading instead of blocking the UI
